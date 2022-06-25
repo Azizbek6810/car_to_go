37c218bdd0da974a50ca79f67d7ede5a92abf360
Xumoyun <rabbimovxumoyun22@gmail.com>
2022-06-25 16:54:08 +0500

    add model page

--- pages/models/index.jsx
+++ pages/models/index.jsx
@@ -6,9 +6,27 @@ const Models=()=>{
     return(
         <div className={styles.models}>
             <Navbar dark/>
-            <div className="w-100">
-            <ModelCard/>
-            </div>
+                <div   className={styles.sidebar}>
+                    <div className={styles.menu}>
+                        <div className={styles.titleMenu}>
+                            <p className="title40 weight7 cl-white">
+                                Brends
+                            </p>
+                        </div>
+                        <div className={styles.menu}>
+                            <ul className="ulMenu"> 
+                                <li>Kia</li>
+                                <li>XPeng</li>
+                                <li>Mercedes Benz</li>
+                                <li>Chevrolet</li>
+                                <li>Skoda</li>
+                            </ul>
+                        </div>
+                    </div>  
+                </div>
+                <div className={styles.main}>
+                    <ModelCard/>
+                </div>
         </div>
     )
 }
